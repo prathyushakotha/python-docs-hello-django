bb7eeebb401990d31018274f6c9d9275fcb26f27
prathyushakotha <70310822+prathyushakotha@users.noreply.github.com>
2021-11-21 19:27:16 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hello everyone! Our Training has come to an end.Wishing everyone all the best for future.")
